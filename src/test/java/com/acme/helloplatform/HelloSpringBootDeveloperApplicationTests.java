0d2d2c1c90f865fe321e9849f7fde69b19a43877
Rafael C <rafael.castro@upc.edu.pe>
2025-09-06 11:07:30 -0500

    chore: initial setup.

---
+++ src/test/java/com/acme/helloplatform/HelloSpringBootDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.helloplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
